1b82802a636380f58700e1d1b9a2cdba6a18f784
Gavin John <gavinnjohn@gmail.com>
2025-10-10 15:31:47 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,125 @@
+{
+  inputs = {
+    ros-overlay = {
+      url = "github:lopsided98/nix-ros-overlay";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros"; # TODO: Don't use a nixpkgs fork
+    systems = {
+      url = "path:./systems/default.nix";
+      flake = false;
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Dependencies
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    inputs:
+    let
+      inherit (inputs) self;
+      inherit (inputs.nixpkgs) lib;
+      distro = "jazzy";
+    in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      flake = {
+        overlays.default = final: prev: {
+          rosPackages = lib.mapAttrs (
+            distro': _:
+            prev.rosPackages.${distro'}
+            // (lib.mapAttrs'
+              (name: value: {
+                name = lib.replaceStrings [ "_" ] [ "-" ] name;
+                inherit value;
+              })
+              (
+                lib.packagesFromDirectoryRecursive {
+                  callPackage = lib.callPackageWith (final // final.rosPackages.${distro'});
+                  directory = ./. + "/src";
+                }
+              )
+            )
+          ) prev.rosPackages;
+        };
+      };
+      perSystem =
+        {
+          self',
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          packages = {
+            rosEnv = pkgs.rosPackages.${distro}.buildEnv {
+              paths =
+                with pkgs.rosPackages.${distro};
+                [
+                  # ROS Core Dependencies
+                  ros-core
+                  ament-cmake
+                  ament-cmake-core
+                  python-cmake-module
+                ]
+                ++ (with pkgs; [
+                  colcon
+                ]);
+            };
+          };
+          treefmt = {
+            programs = {
+              autocorrect.enable = true;
+              nixfmt.enable = true;
+              clang-format.enable = true;
+            };
+            settings.formatter.markdownlint = {
+              enable = true;
+              command = lib.getExe pkgs.markdownlint-cli2;
+              options = [ "--fix" ];
+              includes = [ "*.md" ];
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs =
+              [ self'.packages.rosEnv ]
+              ++ (with pkgs; [
+                httplib
+              ]);
+            buildInputs = (
+              with pkgs;
+              [
+                curl
+              ]
+            );
+          };
+          # https://flake.parts/overlays.html
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.ros-overlay.overlays.default
+              self.overlays.default
+            ];
+            config = { };
+          };
+        };
+      systems = import inputs.systems.outPath;
+    };
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
---
+++ systems/default.nix
@@ -0,0 +1,13 @@
+# lib.systems.flakeExposed
+[
+  "x86_64-linux"
+  "aarch64-linux"
+  "x86_64-darwin"
+  "armv6l-linux"
+  "armv7l-linux"
+  "i686-linux"
+  "aarch64-darwin"
+  "powerpc64le-linux"
+  "riscv64-linux"
+  "x86_64-freebsd"
+]
